5cd56cafdf306e731b923f6d8468523684a60e21
Jessica Rodriguez <jrrodriguez84@gmail.com>
2017-06-20 13:06:13 +0000

    import Task class and refactor eloquent in web route

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,7 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+//import the Task class
+use App\Task;
 
 Route::get('/', function () {
     
@@ -24,7 +16,7 @@ Route::get('/', function () {
 Route::get('/tasks', function () {
     
     //Use Eloquent in a dedicated class
-    $tasks = App\Task::all();
+    $tasks = Task::all();
     
     
     return view('tasks.index', compact('tasks'));
@@ -33,7 +25,7 @@ Route::get('/tasks', function () {
 
 Route::get('/tasks/{task}', function ($id) {
     
-    $task = DB::table('tasks')->find($id);
+    $task = Task::find($id);
     
     return view('tasks.show', compact('task'));
     
